1949635657006bdb28c32bda65ac2bd0e38c12a5
BigBongoJoe <118244573+BigBongoJoe@users.noreply.github.com>
2026-03-11 14:06:36 -0500

    added firebase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,24 @@
-import { useState } from 'react'
 import './App.css'
 
-
-
 function App() {
-  
-
   return (
-    <>
-        <div className="home-page">
-            <div className="left-side-column"></div>
-            <div className="right-side-column"></div>
+    <div className="home-page">
+      <div className="left-side-column"></div>
+      <div className="right-side-column"></div>
 
-            <div className="connect-sign">
-                <h1>Connect</h1>
-            </div>
-            
+      <div className="connect-wrapper">
+        <div className="connect-line"></div>
+
+        <div className="connect-sign">
+          <h1>AlaSolis</h1>
         </div>
-    </>
+      </div>
+
+      <div className="login-box">
+        <p>Username</p>
+        <p>Password</p>
+      </div>
+    </div>
   )
 }
 
